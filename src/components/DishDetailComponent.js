2b4aeef3636eb98fc3b610b2f107d8747f4548f0
Nitin Padalia <padalia.nitin@gmail.com>
2022-02-17 18:42:30 +0530

    week-3-assignment

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbItem } from "reactstrap";
 import { Link } from 'react-router-dom';
+import { Button, Label, Modal, ModalBody, ModalHeader, Row, Col } from "reactstrap";
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
 function RenderDish({ dish }) {
     return (
@@ -15,9 +17,9 @@ function RenderDish({ dish }) {
 
 }
 
-function RenderComments({ dish }) {
-    if (dish != null && dish.comments != null) {
-        const commentsval = dish.comments.map((comment) => {
+function RenderComments({ comments }) {
+    if (comments != null) {
+        const commentsval = comments.map((comment) => {
             return (
                 <li key={comment.id}>
                     <p>{comment.comment}</p>
@@ -35,7 +37,14 @@ function RenderComments({ dish }) {
                 <ul className="list-unstyled">
                     {commentsval}
                 </ul>
+                {
+                    /* Task 1.3: The CommentForm component is used by the 
+                    RenderComments function to display the button for toggling the modal.
+                    */
+                }
+                <CommentForm />
             </div>
+            
         );
     } else {
         return (
@@ -62,7 +71,7 @@ const DishDetail = (props) => {
                     <RenderDish dish={props.dish} />
                 </div>
                 <div className="col-12 col-md-5 m-1">
-                    <RenderComments dish={props.comments} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         </div>
@@ -71,3 +80,118 @@ const DishDetail = (props) => {
 }
 
 export default DishDetail;
+
+/*
+Task 1.1: Add a new class component named CommentForm that will add a button to the view
+*/
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len)
+const minLength = (len) => (val) => val && (val.length >= len);
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            isModalOpen: false
+        };
+    }
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        this.toggleModal()
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+    }
+    render() {
+        return (
+            <React.Fragment>
+                {
+                    /*
+                    Task 1.2: When the button is clicked, it toggles a modal that will display the comment form
+                    */
+                }
+                <Button outline color="secondary" onClick={this.toggleModal}><span className="fa fa-sm fa-pencil"></span> Submit Comment</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        {
+                            /* 
+                            Task 2.1: Set up the form as a local form using the react-redux-form with the 
+                            three fields: author, rating and comment.
+                            Task 2.2: The rating field in the comment form is implemented using a select, 
+                            the author is implemented using a text field, while the comment uses a textarea 
+                            with six rows.
+                            */
+                        }
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Label htmlFor="rating" md={12}>Rating</Label>
+                                <Col md={12}>
+                                    <Control.select model=".userrating" id="rating" name="rating"
+                                        className="form-control"
+                                    >
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5" selected>5</option>
+                                    </Control.select>
+
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="name" md={12}>Your Name</Label>
+                                <Col md={12}>
+                                    <Control.text model=".name" id="username" name="username"
+                                        className="form-control" placeholder="Your Name"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    {
+                                        /*
+                                        Task 3:
+                                        1. The author field should at least be three characters long.
+                                        2. The author field should be less than or equal to 15 characters.
+                                        3. The user should be alerted by showing the invalid message displayed at the
+                                         bottom of the field.
+                                        */
+                                    }
+                                    <Errors
+                                        className="text-danger"
+                                        model=".name"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters.',
+                                            maxLength: 'Must be 15 characters or less.'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="comment" md={12}>Comment</Label>
+                                <Col md={12}>
+                                    <Control.textarea model=".comment" id="comment" name="comment"
+                                        rows="6" className="form-control" />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{ size: 10 }}>
+                                    <Button type="submit" color="primary">Submit</Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+
+        );
+    }
+};
--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = (props) => {
     return (
-        <div className="footer">
+        <div className="light">
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-4 offset-1 col-sm-2">
